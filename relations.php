d6ee7d220625212d3aa6b29b42aa0c1208ee49ce
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-29 09:01:55 +0000

    add entities  type

--- relations.php
+++ relations.php
@@ -33,6 +33,36 @@
             Notez tout de meme qu'n'y a pas besoin de créer àla main user_id. C'est Doctrine qui nous aidera à entretenir cette relation.
             
           </p>
+          <h4>Notion d'unidirectionnalité et de bidirectionnalité :</h4>
+          <p>
+            Cela veut dire qu'une relation peut etre à sens unique ou à double sens . Pour le srelations uniques, il exixtera dans notre exemple de relation entre tache et user, une méthode dans notre netité tache  getUser() qui ira simplement chercher l'utilisateur associé via sa clè etrangère. Dans le cadrde d'une relation bidirectionnelle, il faut expliciter la relation aussi dans l'entité inverse pour que Doctrine , dans cet exemple aille chercher de lui-même toutes les taches d'un utilisateur. Il exixste implicitement une requête pour celz lorsque vous appellerez depuis un user getTasks().
+          </p>
+          <br>
+          
+          <h2>Les types de relations</h2>
+          <p>
+            <ul>
+              <li>OneToOne</li>
+              <li>ManyToOne</li>
+              <li>OneToMay</li>
+              <li>ManuToMay</li>
+            </ul>
+          <br><br>
+          <h4>OneToOne</h4>
+          La relation "1..1" ezst une relation classique .comme son nom l'indique, elle définit une dépendance unique entre deux entités.
+          <br><br>
+          
+          <h4>ManyToOne</h4>
+          Cette relation permet à plusieurs entités "A" d'établir une relation avec une entité "B". Imaginons que vous ayez un blog avec des articles et des commentaires .Il sera possible d'ajouter plusieurs commentaires sur le meme article..Ainsi l'entité commentaire sera liée à l'entité article en relation ManuToOne.
+          <br><br>
+          
+          <h4>OneToMay</h4>
+          Cette entité permet d'établir à une entité "A" une relation avec plusieurs Entités "b". En fait , on se positionne du coté inversee c-à-d que dans l'emple de blog montionné dans la relation ManyToOne on effectue la relation à partir de l'entité article et non de commentaire.
+          <br><br>
+          
+          <h4>ManuToMay</h4>
+          Cette relation peemet à plein d'objet d'etre en relation avec plein d'autres. Cette relation a pour particularité de créer une table de jointure entre les deux entités liées. Cette table ne doit pas etre modifiéé, elle est implicitement créée par doctrine pour retenir les foreignkeys de chacune des entités pour chaque relation entre elles.
+          </p>
      </div>
       </div>
     </div>
